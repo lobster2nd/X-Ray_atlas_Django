808f982ca3c777f5d136e7821fe972b10122fe36
LobsterX <lobster2@mail.ru>
2023-07-27 13:44:50 +0300

    migrations made

--- atlas/urls.py
+++ atlas/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from atlas import settings
 from examinations.views import pageNotFound
 
 urlpatterns = [
@@ -24,4 +26,7 @@ urlpatterns = [
     path('', include('examinations.urls'))
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = pageNotFound
+
--- examinations/models.py
+++ examinations/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Examinations(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    #image = models.ImageField(upload_to="images")   raises error Pillow is not installed
+    image = models.ImageField(upload_to="images")
     video_link = models.URLField()
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
