1e3f475b02e073f1c6f8fad235586bf9397e85e8
lobster <lobster2@mail.ru>
2023-08-12 23:23:39 +0300

    admin panel modified

--- examinations/admin.py
+++ examinations/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import *
 
 
 class ExaminationsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'slug', 'time_create', 'image', 'is_published')
+    list_display = ('id', 'title', 'slug', 'time_create', 'get_html_image', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     prepopulated_fields = {"slug": ("title",)}
+    fields = ('title', 'slug', 'cat', 'content', 'image', 'get_html_image', 'is_published',)
+    readonly_fields = ('time_create', 'time_update', 'get_html_image')
+
+    def get_html_image(self, object):
+        if object.image:
+            return mark_safe(f"<img src='{object.image.url}', width=50>")
+
+    get_html_image.short_description = 'Миниатюра'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -19,3 +28,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Examinations, ExaminationsAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Админ-панель атласа'
+admin.site.site_header = 'Админ-панель атласа'
