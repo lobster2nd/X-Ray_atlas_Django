d9f53c91bd03c0fcd5ec27fa15e4d84e453acd04
LobsterX <lobster2@mail.ru>
2023-09-08 14:12:22 +0300

    api temporarily disabled

--- examinations/models.py
+++ examinations/models.py
@@ -13,7 +13,7 @@ class Examinations(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, blank=False, verbose_name='Область')
-    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.PROTECT)
+    # user = models.ForeignKey('User', verbose_name='Пользователь', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
